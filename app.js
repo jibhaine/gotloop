fa3cdc596189cb0f5a0fbf1d06556668ae280a3e
Benjamin Legrand <jibhaine@gmail.com>
2013-06-09 18:51:08 +0200

    switch to node.js almost ok, more models

---
+++ app.js
@@ -0,0 +1,79 @@
+var log4js = require('log4js');
+//logg configuration
+log4js.configure({
+        appenders: [
+            { type: 'console' },
+            { type: 'file', filename: __dirname +'/server/logs/gotloop.log', category: 'all' },
+            { type: 'file', filename: __dirname +'/server/logs/error.log', category: 'error' }
+        ]}
+);
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , ascii = require('./server/utils/ascii').randomLogo()
+  , routes = require('./server/routes')
+  , models = require('./server/models')
+  , user = require('./server/routes/users')
+  , http = require('http')
+  , fs = require('fs')
+  , path = require('path');
+
+var logger = log4js.getLogger();
+logger.setLevel('INFO');
+
+log4js.replaceConsole(logger);
+
+
+logger.info('Initializing Express...');
+var app = express();
+app.use(log4js.connectLogger(logger, { level: log4js.levels.DEBUG }));
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+//app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//index of the site, main entry point
+app.get('/', routes.index);
+
+//sub site apps / services, accessible via simple html requests.
+//app.get('/login', routes.login);
+//app.get('/upload', routes.upload);
+//app.get('/cutter', routes.cutter);
+//app.get('/boxon', routes.boxon);
+//app.get('/admin', routes.admin);
+
+//partials for angular and ajax requests.
+//TODO .jade partials request
+app.get('/partial/:name',routes.partial);
+//TODO .json  request
+
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'))
+});
+/*
+models.sequelize.sync().error(function(err) {
+    if (err) {
+        logger.error(err);
+        throw err;
+    }
+}).success(function(it) {
+    logger.info(it.length + 'tables successfully modified');
+});
+*/
---
+++ public/js/gotloop.js
@@ -0,0 +1,59 @@
+require.config(
+    {
+        //base url
+        baseUrl:'js/',
+        waitSeconds:20,
+        shim:{},
+        paths:{}
+    }
+);
+
+define(
+    "gotloop",
+    [
+        "../.",
+        "ui-bootstrap",
+        "modernizr"
+    ]
+,function(angular){
+        angular.module('gotLoop',function(){});
+
+	/** HEADER **/
+	
+	/** LOGIN FORM **/
+	//show the login header popin
+	function showLogInField(){
+		
+	}
+	function doLogIn(){
+		
+	}
+	/** SIGNIN FORM **/
+
+	function showSignInField(){
+		
+	}
+	function doSignIn(){
+		
+	}
+	/** LOGOUT **/
+	function doLogOut(){
+		
+	}
+	/** LOOPUPLOAD FORM **/
+	function doLoopUpload(){
+		
+	}
+
+	/** COMMENT FORM **/
+
+	/** LOOP LIST NAV **/
+
+	/** COMMENTS NAV **/
+
+	/** FOLLOWINGS **/
+
+	/** FOLLOWERS **/
+
+	/** FOLLOW **/
+});
