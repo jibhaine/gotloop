ffd9a156ba06089113ebc3035c9d0c8a9da9038c
Benjamin Legrand <jibhaine@gmail.com>
2013-06-10 09:31:40 +0200

    cleaning up in jsp to jade files

--- server/app.js
+++ server/app.js
@@ -1,16 +1,24 @@
-var log4js = require('log4js');
-//logg configuration
-log4js.configure({
+var log4js = require('log4js'),
+    logger = log4js.getLogger('all');
+
+/**
+ * Logging config
+ */
+log4js.configure(
+    {
         appenders: [
             { type: 'console' },
             { type: 'file', filename: __dirname +'/logs/gotloop.log', category: 'all' },
             { type: 'file', filename: __dirname +'/logs/error.log', category: 'error' }
-        ]}
+        ]
+    }
 );
+logger.setLevel('INFO');
+log4js.replaceConsole(logger);
+
 /**
  * Module dependencies.
  */
-
 var express = require('express')
   , ascii = require('./utils/ascii').randomLogo()
   , routes = require('./routes')
@@ -18,28 +26,34 @@ var express = require('express')
   , user = require('./routes/users')
   , http = require('http')
   , fs = require('fs')
-  , path = require('path');
+  , path = require('path')
+  , everyauth = require('everyauth');
 
-var logger = log4js.getLogger();
-logger.setLevel('INFO');
 
-log4js.replaceConsole(logger);
+/**
+ * EveryAuth configuration
+ */
 
+
+/**
+ * Express configuration
+ */
 
 logger.info('Initializing Express...');
 var app = express();
 app.use(log4js.connectLogger(logger, { level: log4js.levels.DEBUG }));
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || '3000');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser('i got a looop'));
 app.use(express.session());
 app.use(app.router);
+app.use(everyauth.middleware());
   app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -48,7 +62,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-//index of the site, main entry point
+
+ //index of the site, main entry point
 app.get('/', routes.index);
 
 //sub site apps / services, accessible via simple html requests.
@@ -61,18 +76,23 @@ app.get('/', routes.index);
 //partials for angular and ajax requests.
 //TODO .jade partials request
 app.get('/partial/:name',routes.partial);
+
 //TODO .json  request
+app.get('/api/:resource',routes.jsonData);
 
+exports.app = app;
 
+if(process.env !== 'TEST'){
+//init server.
 var server = http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'))
 });
+}
+
 /*
+//synchronize ORM with database.
 models.sequelize.sync().error(function(err) {
-    if (err) {
         logger.error(err);
-        throw err;
-    }
 }).success(function(it) {
     logger.info(it.length + 'tables successfully modified');
 });
---
+++ test/func/server-specs.js
@@ -0,0 +1,8 @@
+var app = require('../../server/app'),
+    mocha = require('mocha');
+
+describe('the gotloop Server',function(){
+    before(function(){
+
+    });
+})
