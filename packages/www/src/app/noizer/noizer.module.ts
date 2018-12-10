7370cff371e2312988d1d42f18f63eb3bebf4016
Benjamin Legrand <jibhaine@free.fr>
2018-12-10 09:12:22 +0100

    add modules, dev deps at root level and link projects

---
+++ packages/www/src/app/noizer/noizer.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NoizerRoutingModule } from './noizer-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NoizerRoutingModule
+  ]
+})
+export class NoizerModule { }
