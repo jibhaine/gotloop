6b5ae9091d20f01d9b29033dfbce3170babd8ba1
Benjamin Legrand <benjilegnard@protonmail.com>
2018-12-10 09:12:22 +0100

    add modules, dev deps at root level and link projects

---
+++ packages/www/src/app/loops/loops.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoopsRoutingModule } from './loops-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LoopsRoutingModule
+  ]
+})
+export class LoopsModule { }
