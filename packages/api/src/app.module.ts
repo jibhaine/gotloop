b1204961e6a9419c4f51b0c1de9e6f1e48b37eda
Benjamin Legrand <jibhaine@free.fr>
2018-12-20 00:03:43 +0100

    feat(server): add typorm and modules ?

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { LoopModule } from './loop/loop.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, LoopModule],
   controllers: [AppController],
   providers: [AppService],
 })
