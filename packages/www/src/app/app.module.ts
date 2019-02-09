f0c76bdc43ef142e579cd099daf56f8b532347be
Benjamin Legrand <benjilegnard@protonmail.com>
2019-02-09 20:07:35 +0100

    feat(www): init ngrx/store and hot module reloading

--- packages/www/src/app/app.module.ts
+++ packages/www/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShellModule } from './shell/shell.module';
 import { CoreModule } from './core/core.module';
+import { ShellModule } from './shell/shell.module';
+
+import { reducers, metaReducers } from './reducers';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,6 +24,9 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     CoreModule,
     ShellModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
