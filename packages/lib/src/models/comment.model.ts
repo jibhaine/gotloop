b3a263798f5d530ad4b4e8aec3149747e146808c
Benjamin Legrand <benjamin.legrand.externe@capgemini.com>
2019-04-21 11:23:17 +0200

    fix(lib): fix model exports

--- packages/lib/src/models/comment.model.ts
+++ packages/lib/src/models/comment.model.ts
@@ -1,9 +1,26 @@
 import { JsonProperty } from 'json-typescript-mapper';
 
-class Comment {
-  date: Date;
-  author: string;
+import { UserModel } from "./user.model";
+
+/**
+ * Modelize a user comment on a loop.
+ */
+export class CommentModel {
+
+  @JsonProperty('id')
+  uuid: string;
+
+  @JsonProperty('date')
+  createdAt: Date;
+
+  @JsonProperty('author')
+  author: UserModel;
+
+  @JsonProperty('content')
   content: string;
+
+  @JsonProperty('avatar')
+  avatar: string;
 }
 
 export default Comment;
