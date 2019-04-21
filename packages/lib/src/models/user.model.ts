da80b8bd4a734bb875126141f89fa84c02fabc49
Benjamin Legrand <benjilegnard@protonmail.com>
2019-04-21 11:23:17 +0200

    fix(lib): fix model exports

--- packages/lib/src/models/user.model.ts
+++ packages/lib/src/models/user.model.ts
@@ -3,4 +3,7 @@ import { JsonProperty } from 'json-typescript-mapper';
 export class UserModel {
   @JsonProperty('name')
   public name: string;
+  @JsonProperty('avatar')
+  public avatar: string;
+
 }
