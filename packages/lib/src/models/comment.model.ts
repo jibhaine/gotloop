7dcb6b95fd66f37a4b349492efa7552362c16797
Benjamin Legrand <benjilegnard@protonmail.com>
2019-11-24 09:06:20 +0100

    fix(): comment usages on JsonProperty (not compatible typescript 3.x?)

--- packages/lib/src/models/comment.model.ts
+++ packages/lib/src/models/comment.model.ts
@@ -1,21 +1,15 @@
-import { JsonProperty } from 'json-typescript-mapper';
-
 import { UserModel } from './user.model';
 
 /**
  * Modelize a user comment on a loop.
  */
 export class CommentModel {
-  @JsonProperty('uuid')
   public uuid: string;
 
-  @JsonProperty('date')
   public createdAt: Date;
 
-  @JsonProperty('author')
   public author: UserModel;
 
-  @JsonProperty('content')
   public content: string;
 
 }
--- packages/lib/src/models/user.model.ts
+++ packages/lib/src/models/user.model.ts
@@ -1,9 +1,6 @@
-import { JsonProperty } from 'json-typescript-mapper';
 
 export class UserModel {
-  @JsonProperty('name')
   public name: string;
-  @JsonProperty('avatar')
   public avatar: string;
 
 }
