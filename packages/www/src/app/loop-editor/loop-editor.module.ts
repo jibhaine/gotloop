ed5b93cf78eb2da96d15e730ef3c54291ed3d057
Benjamin Legrand <benjilegnard@protonmail.com>
2019-11-25 07:23:00 +0100

    stash(): small changes

--- packages/www/src/app/loop-editor/loop-editor.module.ts
+++ packages/www/src/app/loop-editor/loop-editor.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
 import { LoopCutterComponent } from './components/loop-cutter/loop-cutter.component';
 import { LoopUploadComponent } from './components/loop-upload/loop-upload.component';
 import { LoopRecordComponent } from './components/loop-record/loop-record.component';
 import { LoopEditorComponent } from './components/loop-editor/loop-editor.component';
-import { StoreModule } from '@ngrx/store';
-import * as fromLoopEditor from './reducers/loop-editor.reducer';
-import { EffectsModule } from '@ngrx/effects';
-import { LoopEditorEffects } from './effects/loop-editor.effects';
 import { LoopFormComponent } from './components/loop-form/loop-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoopEditorRoutingModule } from './loop-editor-routing.module';
+import { LoopEditorEffects } from './effects/loop-editor.effects';
+import * as fromLoopEditor from './reducers/loop-editor.reducer';
 
 @NgModule({
   declarations: [LoopCutterComponent, LoopUploadComponent, LoopRecordComponent, LoopEditorComponent, LoopFormComponent],
--- packages/www/src/app/noizr/reducers/noizr.reducer.ts
+++ packages/www/src/app/noizr/reducers/noizr.reducer.ts
@@ -1,12 +1,14 @@
 
 import { NoizrActions, NoizrActionTypes } from '../actions/noizr.actions';
+import { Settings } from '@gotloop/lib';
 
 export interface State {
-
+  id?: string;
+  settings: Settings[];
 }
 
 export const initialState: State = {
-
+  settings: [],
 };
 
 export function reducer(state = initialState, action: NoizrActions): State {
