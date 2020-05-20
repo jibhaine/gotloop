383a8608b5ff9bbfe782d2f8713f772c027f76f2
Benjamin Legrand <benjilegnard@protonmail.com>
2020-05-20 11:21:33 +0200

    feat(simpl): move box & cable components

--- apps/www/src/app/noizr/noizr.module.ts
+++ apps/www/src/app/noizr/noizr.module.ts
@@ -6,20 +6,21 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
 import { NoizerRoutingModule } from './noizr-routing.module';
-import { BoxComponent } from './components/box/box.component';
-import { CableComponent } from './components/cable/cable.component';
 import { NoizerPageComponent } from './components/noizer-page/noizer-page.component';
 import { ShellModule } from '../shell/shell.module';
 import * as fromNoizr from './reducers/noizr.reducer';
 import { NoizrEffects } from './effects/noizr.effects';
 
+import { GraphModule } from '@gotloop/simpl';
+
 @NgModule({
-  declarations: [BoxComponent, CableComponent, NoizerPageComponent],
+  declarations: [NoizerPageComponent],
   imports: [
     CommonModule,
     NoizerRoutingModule,
     ShellModule,
     AngularDraggableModule,
+    GraphModule,
     StoreModule.forFeature('noizr', fromNoizr.reducer),
     EffectsModule.forFeature([NoizrEffects]),
   ],
